38e6559021b864c21374e9ee4b3d9d38357f10ed
Seyha Rouge 29 <99944939+Bonker009@users.noreply.github.com>
2025-06-25 19:12:56 +0700

    d

--- renthouse-ui/src/lib/api.ts
+++ renthouse-ui/src/lib/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { getCookie, removeCookie } from './cookies';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://96.9.81.187:9024/api';
+ 
 
 console.log('API Base URL:', API_BASE_URL);
 
